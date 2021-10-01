0bf63afa2c6f0db7bb4248b21d3a4cfc42b518b0
pranav <prnvkatkar88@gmail.com>
2021-10-01 10:43:32 +0530

    Added:code to find leap year

---
+++ main/java/com/basiccoreprograms/LeapYear.java
@@ -0,0 +1,36 @@
+package com.basiccoreprograms;
+
+import java.util.Scanner;
+
+public class LeapYear
+{
+	public static void main(String[] args)
+	{
+		int year;
+ 	    Scanner sc = new Scanner(System.in);   
+		System.out.print("enter year :");
+		year=sc.nextInt();
+ 	    leapOrNot(year);
+ 	    sc.close();
+	}
+		
+		public static void leapOrNot(int year)
+		{
+			if(year!=0)
+			{
+			   if(year%400==0)
+		                  	System.out.println(year+" is a leap year");
+		                  else  if(year%100==0)
+			    	System.out.println(year+" is not a leap year");
+		                   	else if(year%4==0)                    
+			    		System.out.println(year+" is a leap year");
+		                     	       else 
+			   		 System.out.println(year+" is not a leap year");                       
+		 	 }
+			                       
+		 	}
+		}
+
+	
+
+
